0a4e4a9d7228fc8f6c51e5074b14e0c032c94772
Georgii Naumov <gmnaumov@yandex.ru>
2024-11-11 21:47:40 +0300

    feat: adding saving of entities to the database

--- src/main/java/com/ngm/models/Todo.java
+++ src/main/java/com/ngm/models/Todo.java
@@ -1,9 +1,11 @@
 package com.ngm.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -11,6 +13,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "todos")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -18,12 +21,16 @@ import lombok.Setter;
 @EqualsAndHashCode
 public class Todo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "is_completed")
     private boolean isCompleted;
 }
