ebd91caffdca6e30fbd73209f43f70f253a6d7a8
Georgii Naumov <gmnaumov@yandex.ru>
2024-11-18 20:35:14 +0300

    feat: added validation on Todo creation

--- src/main/java/com/ngm/controllers/TodoController.java
+++ src/main/java/com/ngm/controllers/TodoController.java
@@ -1,30 +1,41 @@
 package com.ngm.controllers;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ngm.models.Todo;
 import com.ngm.repositories.TodoRepository;
+
+import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 public class TodoController {
-    @Autowired
     private TodoRepository todoRepository;
+
+    public TodoController(@Autowired TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
 
     @GetMapping("/hello")
     public String greeting() {
@@ -32,9 +43,28 @@ public class TodoController {
     }
 
     @PostMapping("/todos")
-    public ResponseEntity<Todo> createTodo(@RequestBody Todo todo) {
+    public ResponseEntity<Todo> createTodo(@Valid @RequestBody Todo todo, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+
+            bindingResult.getAllErrors()
+            .forEach(error -> {
+                String fieldName = ((FieldError)error).getField();
+                String errorCode = error.getDefaultMessage();
+                errors.put(fieldName, errorCode);
+            });
+            
+            return ResponseEntity.badRequest().build();
+        }
+
         Todo newTodo = todoRepository.save(todo);
-        return new ResponseEntity<>(newTodo, HttpStatus.OK);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newTodo.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newTodo);
     }
 
     @GetMapping("/todos")
@@ -44,7 +74,7 @@ public class TodoController {
     }
 
     @GetMapping("/todos/{id}")
-    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+    public ResponseEntity<Todo> getTodoById(@PathVariable("id") Long id) {
         Optional<Todo> todo = todoRepository.findById(id);
 
         if (todo.isPresent()) {
@@ -55,7 +85,7 @@ public class TodoController {
     }
 
     @PutMapping("/todos/{id}")
-    public ResponseEntity<Todo> updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+    public ResponseEntity<Todo> updateTodo(@PathVariable("id") Long id, @RequestBody Todo todo) {
         return todoRepository.findById(id)
                 .map(existingTodo -> {
                     existingTodo.setTitle(todo.getTitle());
@@ -80,7 +110,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/todos/{id}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteTodo(@PathVariable("id") Long id) {
         Optional<Todo> existingTodo = todoRepository.findById(id);
 
         if (existingTodo.isPresent()) {
